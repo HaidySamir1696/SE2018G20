c8f833c061b263090803bbd84bf8d54cba8ab996
HaidySamir1696 <36296063+HaidySamir1696@users.noreply.github.com>
2019-02-11 01:51:55 +0200

    Update and rename projects(updated).php to projects(updated version2).php

--- backend/projects(updated version2).php
+++ backend/projects(updated version2).php
@@ -7,34 +7,33 @@ class Project extends Database{
 	
 	function __construct($ProjectId)
 	{
-		$sql="SELECT * FROM project WHERE ProjectId=$ProjectId";
+		$sql="SELECT * FROM projects WHERE ProjectId=$ProjectId";
 		$statement= Database::$db->prepare($sql);
 		$statement->execute();
 		$data=$statement->fetch(PDO::FETCH_ASSOC);
 		foreach ($data as $key => $value) {
 			$this->{key}=$value;
 		}
-	
+
+	         //$url=$this->parse_url();
+		print_r($this->parse_url());
 	}
 
+    public static function parse_url(){
+
+		
+		if(isset($_GET['url']))
+		{
+
+			return $url=explode('/', filter_var(rtrim($_GET['url'],'/'),FILTER_SANITIZE_URL));
+		}
+	}
 //Create a project by the owner
 	public static function CreateProject($projectName,$category,$location,$description,$rating,$pImage){
 
 		$sql="INSERT INTO project (name, category, location, description, rating, primary_image) VALUES (?, ?, ?, ?, ?, ?)";
 		Database::$db->prepare($sql)->execute([$projectName,$category,$location,$description,$rating,$pImage]);
 	}
-
-
-// create a project by the owner
-
-	/*public static function CreateProject($projectName,$category_id,$owner_id){
-
-		$sql="INSERT INTO project(projectName,category_id,owner_id) values(?,?,?)  ";
-		$statement= Database::$db->prepare($sql);
-		$statement->execute([$projectName,$category_id,$owner_id]);
-	}*/
-
-	
 
 //delete project
 
@@ -52,21 +51,14 @@ class Project extends Database{
 		$statement= Database::$db->prepare($sql);
 		$statement->execute([$this->projectName, $this->category_id,$this->owner_id]);
 	}
+public static function CreateProject($projectName,$category,$location,$description,$rating,$pImage){
 
+	public static function AddFeedback($ProjectID,$customerID,$feedback){
 
+$sql = "INSERT INTO feedbacks(Project_ID,customer_id,feedback) VALUES (.$projectid,.$id,?)";
+Database::$db->prepare($sql)->execute([$ProjectID,$customerID,$feedback]);
 }
 
-	/*public static function all($keyword) {
-		
-		$keyword = str_replace(" ", "%", $keyword);
-		$sql = "SELECT * FROM students WHERE name like ('%$keyword%');";
-		$statement = Database::$db->prepare($sql);
-		$statement->execute();
-		$students = [];
-		while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-				$students[] = new Student($row['id']);
-		}
-		return $students;
-	}*/
+}
 
 ?>
