3992e27fc52d0ce79955e9c1834796712faee4e8
HaidySamir1696 <36296063+HaidySamir1696@users.noreply.github.com>
2019-02-14 18:53:21 +0300

    Add files via upload

--- backend/restaurant.php
+++ backend/restaurant.php
@@ -1,8 +1,13 @@
-<?php include_once('./common/headwith.php') ;
+<?php 
+ include_once('./common/headwith.php') ;      
+ include_once ('./models/Database.php');
+ include_once('./controllers/common.php');
+ include_once ('./models/projects.php');
+ Database::connect('startups_on_the_cloud','root',''); 
+ // $projectid=$_POST['projectid'];
+ // $feedback =safeGet("textArea");
+ //Project::AddFeedback($projectid,$feedback);
 
-               include_once ('./models/Database.php');
-
-                Database::connect('startups_on_the_cloud','root','');
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +23,7 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Open Sans", sans-serif}
 
 
 <!-- Page Container -->
-<div class="w3-container w3-content" style="max-width:1400px;margin-top:80px">
+<div class="w3-container w3-content" style="max-width:1400px;margin-top:80px">    
   <!-- The Grid -->
   <div class="w3-row">
     <!-- Left Column -->
@@ -33,7 +38,7 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Open Sans", sans-serif}
         </div>
       </div>
       <br>
-
+      
 
       <div class="w3-card w3-round w3-white">
         <div class="w3-container">
@@ -41,40 +46,44 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Open Sans", sans-serif}
          <img src="img/food.jpg" alt="Restaurant" width="300" height="300">
         </div>
       </div>
-      <br>
+      <br>        
       <!-- Accordion -->
       <div class="w3-card w3-round">
         <div class="w3-white">
-
+       
           <button  class="w3-button w3-block w3-theme-l1 w3-left-align"><i class="fa fa-calendar-check-o fa-fw w3-margin-right"></i> Rates</button>
-
-          <button onclick="myFunction('Demo3')" class="w3-button w3-block w3-theme-l1 w3-left-align"><i class="fa fa-users fa-fw w3-margin-right"></i> Locations</button>
+          
+          <button onclick="myFunction('Demo3')" class="w3-button w3-block w3-theme-l1 w3-left-align"><i class="fa fa-users fa-fw w3-margin-right"></i> Locations (eg:shobra,Madii,Moaatam)</button>
           <div id="Demo3" class="w3-hide w3-container">
             <div class="w3-row-padding">
               <br>
-                   <div >
-                      <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom">Â Madenit Nasr</button>
+              <form action="" class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" name="keywords" placeholder="Search Location" aria-label="Search" value="<?=safeGet('keywords')?>">
+            <button class="w3-button w3-block w3-theme-l1 w3-left-align" type="submit">Search</button>
+          </form>
+                 <!--   <div >
+                      <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"> Madenit Nasr</button> 
                    </div>
                    <div >
-                    <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom">Â Masr El Gdeda</button>
+                    <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"> Masr El Gdeda</button> 
                    </div>
                    <div >
-                     <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"> Moaatam</button>
-                   </div>
+                     <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"> Moaatam</button> 
+                   </div> 
                    <div >
-                     <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"> Madii</button>
+                     <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"> Madii</button> 
                    </div>
                    <div>
-                     <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"> shobra</button>
-                   </div>
+                     <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"> shobra</button> 
+                   </div> -->
 
             </div>
           </div>
-        </div>
+        </div>      
       </div>
       <br>
-
-      <!-- Interests -->
+      
+      <!-- Interests --> 
       <div class="w3-card w3-round w3-white w3-hide-small">
         <div class="w3-container">
           <p>category</p>
@@ -93,68 +102,110 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Open Sans", sans-serif}
           </p>
         </div>
       </div>
-
+      
     <!-- End Left Column -->
     </div>
-    <?php
 
-                          //  $sql="SELECT * FROM startup_owner WHERE id=$id";
-                          //  $statement= Database::$db->prepare($sql);
-                          //$statement->execute();
-                           // $data=$statement->fetch(PDO::FETCH_ASSOC);
-                          $query = "SELECT * FROM project WHERE category='restaurant' ";
-                          $data = Database::$db->query($query);
-                          $data->execute();
-                          while($row=$data->fetch(PDO::FETCH_ASSOC)):
-                           ?>
-    <!-- Middle Column -->
     <div class="w3-col m7">
 
+    <?php
+     
+        //$projectrow = Project::searchlocation(safeGet('keywords'));
+        //echo safeGet('keywords');
+       // foreach ($projectrow as $prow) {
+      
 
+                          //  $sql="SELECT * FROM startup_owner WHERE id=$id";
+                            //$statement= Database::$db->prepare($sql);
+                          //$statement->execute();
+
+                            //$data=$statement->fetch(PDO::FETCH_ASSOC);
+                          // $query = "SELECT * FROM project WHERE category='restaurant' ";
+                           //$data = Database::$db->query($query);
+                           //$data->execute();
+                           //while($row=$data->fetch(PDO::FETCH_ASSOC)):
+                            //$id=$row['ProjectID'];
+      $keyword=safeGet('keywords');
+      $keyword = str_replace(" ", "%", $keyword);
+      $sql="SELECT * FROM `project` WHERE category='Restaurant' AND location LIKE ('%$keyword%');";
+
+      $statement = Database::$db->prepare($sql);
+      $statement->execute();
+      $projectrow = [];
+      while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $projectrow[] = new Project($row['ProjectID']);
+      
+
+                          //    $projectrow = Project::searchlocation(safeGet('keywords'));
+                    //$sql="SELECT * FROM 'project' WHERE category='Restaurant' AND location LIKE ('%keyword%');";
+
+                     //$statement = Database::$db->prepare($sql);
+                     //$statement->execute();
+                     //$projectrow = [];
+                      //while($row = $statement->fetch(PDO::FETCH_ASSOC)):
+                     // $projectrow[] = new Project($row['Project_ID']);
+                    //$projectrow = Project::searchlocation(safeGet('keywords'));
+                           // Project::AddFeedback($id,$feedback);
+  ?> 
+    <!-- Middle Column -->
+
+    
+      
       <div class="w3-container w3-card w3-white w3-round w3-margin"><br>
-        <img src="primary_image/<?php echo $row['primary_image']; ?>"  class="w3-left w3-circle w3-margin-right" style="width:100px">
+        <img src="img/#" alt="photo" class="w3-left w3-circle w3-margin-right" style="width:60px">
+        
 
         <h4><?php echo  $row['name']."<br><br>";  ?></h4><br>
-
+      
         <hr class="w3-clear">
         <p><?php echo $row['description']."<br><br>";  ?></p>
            <form id="view" method="post" action="project.php">
-                  <input type="hidden" id="projectid" name="projectid" value= "<?php echo $row['Project_ID'];?>" >
-                                <input class="w3-button w3-theme-d1 w3-margin-bottom" type="submit" id="viewproject" name="view" value = "View">
-                            </form>
-
+                  <input type="hidden" id="projectid" name="projectid" value= "<?php echo $row['Project_ID'];?>" ><div class="w3-container w3-card w3-white w3-round w3-margin"><br>
+                  <input class="w3-button w3-theme-d1 w3-margin-bottom" type="submit" id="viewproject" name="view" value = "View">
+                            </form>       
+<!-- <form method="post" action="" >
+       <input style="width:100%; height:50%;" placeholder="Place your gentle feedback" name="textArea">
+       
+       <br>
+        <input type="Submit" name="SubmitFeedback" class="w3-button w3-theme-d1 w3-margin-bottom" value="add feedback">
+    
+       </form> -->
+                                   
+        
       </div>
-
-     <?php  endwhile; ?>
-
+      
+     <?php  
+       }//endwhile; 
+     ?>
+      
     <!-- End Middle Column -->
     </div>
-
+   
     <!-- Right Column -->
     <div class="w3-col m2">
       <div class="w3-card w3-round w3-white w3-center">
-        <div class="w3-container">
-          <p>Place of the day</p>
-          <img src="/w3images/forest.jpg" alt="place of the day" style="width:100%;">
-          <p><strong>restaurant name </strong></p>
-          <p><button class="w3-button w3-block w3-theme-l4">Info</button></p>
-        </div>
-      </div>
-      <br>
-
-
-
+           <div class="w3-container">
+           <p>Place of the day</p>
+           <img src="/w3images/forest.jpg" alt="place of the day" style="width:100%;">
+           <p><strong>restaurant name </strong></p>
+           <p><button class="w3-button w3-block w3-theme-l4">Info</button></p>
+            </div>
+    </div>
+    <br>
+      
+      
+      
     <!-- End Right Column -->
     </div>
-
-
+    
+    
   <!-- End Grid -->
   </div>
-
+  
 <!-- End Page Container -->
 </div>
 
-
+ 
 <script>
 // Accordion
 function myFunction(id) {
@@ -162,9 +213,9 @@ function myFunction(id) {
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
         x.previousElementSibling.className += " w3-theme-d1";
-    } else {
+    } else { 
         x.className = x.className.replace("w3-show", "");
-        x.previousElementSibling.className =
+        x.previousElementSibling.className = 
         x.previousElementSibling.className.replace(" w3-theme-d1", "");
     }
 }
@@ -174,7 +225,7 @@ function openNav() {
     var x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
-    } else {
+    } else { 
         x.className = x.className.replace(" w3-show", "");
     }
 }
