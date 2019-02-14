722b1207989a180083bf69374bf58a4b38d8431d
Esraa1moshsen <36335832+Esraa1moshsen@users.noreply.github.com>
2019-02-14 14:32:43 +0200

    Add files via upload
    5ali balek mn tables

--- backend/signupowner.php
+++ backend/signupowner.php
@@ -1,16 +1,13 @@
-<?php
- include_once('./common/headwithout.php');
- include_once('./models/customers.php');
- include_once('./models/owners.php');
- include_once ('./controllers/signupFormControl.php');
- include_once ('./controllers/signupformcontrolowner.php');
+<?php include_once('./common/headwithout.php');
+//include_once('./models/customers.php');
+//include_once('./models/owners.php');
+//include_once ('./controllers/signupFormControl.php');
+ //include_once ('./controllers/signupformcontrolowner.php');
  include_once('./models/Database.php');
- Database::connect('startups_on_the_cloud','root','');
+ Database::connect('startups_in_the_cloud','root','turtledove');
 
-  $msg = "";
   if (isset($_POST['submit'])) {
-
-  	$primary_image =$_FILES['primary_image']['name'];
+    	$primary_image =$_FILES['primary_image']['name'];
         $primary_target = "primary_image/".basename($primary_image);
         $secondary_image_1=$_FILES['secondary_image_1']['name'];
         $secondary_target_1 = "secondary_image/".basename($secondary_image_1);
@@ -22,9 +19,10 @@
         $secondary_target_4 = "secondary_image/".basename($secondary_image_4);
         $secondary_image_5=$_FILES['secondary_image_5']['name'];
         $secondary_target_5 = "secondary_image/".basename($secondary_image_5);
-  	$sql = "INSERT INTO project(primary_image,secondary_photo_1,secondary_photo_2,secondary_photo_3,secondary_photo_4,secondary_photo_5) VALUES ('$primary_image','$secondary_image_1','$secondary_image_2','$secondary_image_3','$secondary_image_4','$secondary_image_5')";
+
+  	$sql = "INSERT INTO secondary_photos(primary_image,secondary_photo_1,secondary_photo_2,secondary_photo_3,secondary_photo_4,secondary_photo_5) VALUES ('$primary_image','$secondary_image_1','$secondary_image_2','$secondary_image_3','$secondary_image_4','$secondary_image_5')";
         $data=Database::$db->query($sql);
-        $data->execute();
+
        	if (move_uploaded_file($_FILES['primary_image']['tmp_name'], $primary_target)) {
   		$msg = "Image uploaded successfully";
   	            }else{
@@ -35,34 +33,27 @@
   	              }else{
   		$msg = "Failed to upload image";
   	}
-
        	if (move_uploaded_file($_FILES['secondary_image_2']['tmp_name'], $secondary_target_2)) {
   		$msg = "Image uploaded successfully";
   	              }else{
   		$msg = "Failed to upload image";
   	}
-
        	if (move_uploaded_file($_FILES['secondary_image_3']['tmp_name'], $secondary_target_3)) {
   		$msg = "Image uploaded successfully";
   	              }else{
   		$msg = "Failed to upload image";
   	}
-
        	if (move_uploaded_file($_FILES['secondary_image_4']['tmp_name'], $secondary_target_4)) {
   		$msg = "Image uploaded successfully";
   	              }else{
   		$msg = "Failed to upload image";
   	}
-
        	if (move_uploaded_file($_FILES['secondary_image_5']['tmp_name'], $secondary_target_5)) {
   		$msg = "Image uploaded successfully";
   	              }else{
   		$msg = "Failed to upload image";
   	}
-
-
   }
-
  ?>
 
 
@@ -135,7 +126,7 @@ textarea{
 <br><br>
 
 <div class="main">
-     <form method="POST" action="signupowner.php" enctype="multipart/form-data"" >
+    <form  method="post"  enctype="multipart/form-data"" >
 
     <label> Name :  </label>
             <input class="signup-info" type="text" placeholder="Name" id="username2" name="username2"><br><hr>
@@ -147,11 +138,11 @@ textarea{
             <input  class="signup-info" type="tel" name="phonenumber" placeholder="phonenumber" id="phonenumber"><br><hr>
 
     <label> Project Name </label>
-    <input type="text" id="project_name" class="signup-info" placeholder="Enter Project Name" ><br/>
+    <input type="text" id="project_name" class="signup-info" placeholder="Enter Project Name" name="projectName"><br/>
     <hr>
 
     <label> Category </label>
-    <select class="signup-info">
+    <select class="signup-info" name="cate">
     <option value="Restaurant" selected>Restaurant</option>
     <option value="Shopping" >Shopping</option>
     <option value="Games and Entertainment" >Games and Entertainment</option>
@@ -165,11 +156,9 @@ textarea{
     <input type="file" name="primary_image"> <br/>
         <br/>
     <hr>
-
     <label>Upload secondary Photo 1</label>
     <input type="file" name="secondary_image_1"> <br/>
         <br/>
-
     <hr>
     <label>Upload secondary Photo 2</label>
     <input type="file" name="secondary_image_2"> <br/>
@@ -187,18 +176,19 @@ textarea{
     <input type="file" name="secondary_image_5"> <br/>
         <br/>
                 <hr>
-    <label> Location URL </label>
-    <input type= "url" id="location" class="signup-info" placeholder="Enter Google Maps URL"><br/>
-    <hr>
 
-    <label> Startup Description</label><br/>
-    <textarea style="width:100%" id="Startup_Description" placeholder="Enter Startup Description"></textarea>
+    <label> Location URL </label>
+    <input type= "url" id="location" class="signup-info" placeholder="Enter Google Maps URL" name="location"><br/>
     <hr>
-    <label> National ID </label>
+        
+    <label> Startup Description</label><br/>
+    <textarea style="width:100%" id="Startup_Description" placeholder="Enter Startup Description" name="description"></textarea>   
+    <hr>
+    <label> National ID </label>  
     <input type="text" id="project_name" class="signup-info" maxlength="14" placeholder="Enter National ID" name="nationalID"><br/>
     <hr>
-
-    <input type="submit" value="submit" name="submit" id="submit" class="signup-info">
+        
+    <input type="submit" value="submit" id="submit" class="signup-info">
         <br><br><br>
     </form>    
 </div>
